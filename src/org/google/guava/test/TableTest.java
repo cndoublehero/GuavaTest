290a724e28ac81a9edf499113d0bffafd373d467
cndoublehero <cndoublehero@gmail.com>
2013-07-11 21:55:56 +0800

    add custom table impl

--- src/org/google/guava/test/TableTest.java
+++ src/org/google/guava/test/TableTest.java
@@ -7,8 +7,12 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import com.google.common.base.Supplier;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Table;
+import com.google.common.collect.Tables;
 
 public class TableTest {
 
@@ -36,6 +40,43 @@ public class TableTest {
 		System.out.println(table);
 		System.out.println(table.row("cib"));
 		Map<String, Map<String, String>> map = table.rowMap();
+		Collections2.orderedPermutations(map.keySet());
 		System.out.println(map);
 	}
+
+	@Test
+	public void testTable2() {
+		Table<String, String, String> table = Tables.newCustomTable(
+				Maps.<String, Map<String, String>> newTreeMap(),
+				new Supplier<Map<String, String>>() {
+					@Override
+					public Map<String, String> get() {
+						return Maps.newHashMap();
+					}
+				});
+		table.put("cib", "1004", "2");
+		table.put("cib", "1005", "3");
+		table.put("cib", "1006", "1");
+		table.put("cib", "1007", "5");
+
+		table.put("icbc", "1004", "2");
+		table.put("icbc", "1005", "3");
+		table.put("icbc", "1006", "1");
+		table.put("icbc", "1007", "5");
+
+		assertEquals(8, table.size());
+		assertTrue(table.containsRow("cib"));
+		Set<String> set = table.rowKeySet();
+		assertNotNull(set);
+		assertEquals(2, set.size());
+		assertTrue(set.contains("cib"));
+		assertTrue(set.contains("icbc"));
+		assertTrue(!set.contains("1cib"));
+		System.out.println(table);
+		System.out.println(table.row("cib"));
+		Map<String, Map<String, String>> map = table.rowMap();
+		Collections2.orderedPermutations(map.keySet());
+		System.out.println(map);
+	}
+
 }
